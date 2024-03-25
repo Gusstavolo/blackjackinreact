b25fbb2271ba84c04b3f1a2bc66cf47a5ab9a6f7
Gustavo Oli <69ggustavoo69@gmail.com>
2024-03-25 13:15:53 -0300

    fixed cards

--- src/components/GenerateCards.jsx
+++ src/components/GenerateCards.jsx
@@ -1,75 +1,54 @@
-import '../App.css';
-import { createElement } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import React, { useState } from "react";
 
-let ChangerCard = ('./models/cards/cardsss.png')
-
-
-export const CardStyle = styled.div`
-        display: flex;
-        width: 150px;
-        height: 230px;
-        border-radius: 10%;
-        background: black;
-        
+const CardStyle = styled.div`
+    display: flex;
+    width: 150px;
+    height: 230px;
+    border-radius: 10%;
+    background: black;
 `;
+
 const CardStyleH1 = styled.h1`
-
-color: white;
-
+    font-size: 4rem;
+    color: white;
 `;
-
-
 
 export const ButtonCard = ({ onClick }) => {
-  return (
-    <button onClick={onClick}>
-      Click me
-    </button>
-  );
+    return (
+        <button onClick={onClick}>
+            Click me
+        </button>
+    );
 }
 
 export const GenerateCards = () => {
-    
-    
-    
-    
     const [cards, setCards] = useState([]);
-  
+    console.log("Valores das cartas:", cards);
     const randomNumberInRange = (min, max) => {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     };
-  
+
     const handleClick = () => {
-      const num = randomNumberInRange(1, 13); // Gerar o número antes de definir o card
-      setCards([...cards, test(num)]); // Adicionar um novo card com o número gerado
+        const num = randomNumberInRange(1, 13); // Gerar o número antes de definir o card
+        const newCard = { id: Date.now(), num }; // criar um novo objeto carta com um id único baseado no timestamp atual
+        setCards([...cards, newCard]); // Adicionar o novo card à matriz de cards
     }
-  
-    const test = (num) => {
-      return (
-        <div className='card'>
-          <CardStyleH1>
-            {num}
-          </CardStyleH1>
-        </div>
-      );
-    }
-  
-
-
-
 
     return (
-    <><ButtonCard onClick={handleClick} />
-      
-        <div className='OrganizeCards'>
-            
+        <>
+            <ButtonCard onClick={handleClick} />
+            <div className='OrganizeCards'>
                 {cards.map((card, index) => (
-                <div key={index}>{card}</div>
-            ))}
-                
-        </div>
-    </>
-    )
+                    <div key={card.id} className='card'>
+                        
+                            <CardStyleH1>
+                                {card.num}
+                            </CardStyleH1>
+                       
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 }
