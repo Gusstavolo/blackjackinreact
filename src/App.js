9d52862180e6b6f0dd15eca236bbd3f48e0b4b25
Gustavo Oli <69ggustavoo69@gmail.com>
2024-03-25 21:22:12 -0300

    fixed card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
-import { GenerateCards } from './components/GenerateCards';
 import { Canvas } from '@react-three/fiber';
 import { Experience } from './components/Experience';
-import { CanvasCardGen } from './components/GenerateCards';
 import { OrbitControls } from '@react-three/drei';
+import React, { useRef, useState, createContext, useContext } from 'react';
+import styled from 'styled-components';
+import { CardsLOt } from './components/mesa.jsx';
+
+
+const CardStyleH1 = styled.h1`
+    font-size: 4rem;
+    color: white;
+`;
 
 function App() {
+
+  const randomNumberInRange = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+    const mesh = useRef();
+    const [cards, setCards] = useState([]);
+
+    console.log("Valores das cartas:", cards);
+
+    const CardClick = ({ onClick }) => {
+        const mesh = useRef();
+        const [hovered, setHover] = useState(false);
+    
+        return (
+            <CardsLOt
+                ref={mesh}
+                onClick={onClick}
+            ></CardsLOt>
+        );
+    };
+    
+    const handleClick = () => {
+        const num = randomNumberInRange(1, 13);
+        const newCard = { id: Date.now(), num };
+        setCards([...cards, newCard]);
+    };
+
+
+
+
   return (
     <div className="App">
       <div className='CanvasS'> 
@@ -15,13 +52,23 @@ function App() {
                 <Experience>
                   
                 </Experience>
-            <CanvasCardGen></CanvasCardGen>
-            </Canvas>
+                <CardsLOt onClick={handleClick}/>
+        </Canvas>
       </div>
       
-      <footer>
-        <GenerateCards />
-      </footer>
+        
+          <footer>
+          <div className='OrganizeCards'>
+                {cards.map((card, index) => (
+                    <div key={card.id} className='card'>
+                        <CardStyleH1>
+                            {card.num}
+                        </CardStyleH1>
+                    </div>
+                ))}
+            </div>
+          </footer>
+          
          
      
     </div>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,6 @@
 
 import { SpotLight,AmbientLight, PointLight } from "three";
 import { MESA, CardsLOt } from "./mesa.jsx";
-import { GenerateCards } from "./GenerateCards.jsx";
 import { useRef } from "react";
 import { useState } from "react";
 export const Experience = () => {
@@ -13,7 +12,6 @@ export const Experience = () => {
             <pointLight position={[0,0,3.8]} intensity={7} />
             <MESA></MESA>
            
-            
             
             
             
